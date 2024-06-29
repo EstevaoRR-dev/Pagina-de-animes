7727b25014a78eb10f0f7468417c2ec4ab84defc
Estêvão Roldão <estevaor_roldao@hotmail.com>
2024-06-29 19:05:16 -0300

    Adicionei o quadro e o complete char nos quizzes fruit e wanted

--- src/app/menu/fruit-quiz/fruit-quiz.component.ts
+++ src/app/menu/fruit-quiz/fruit-quiz.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { GameHeaderComponent } from "../../header/game-header/game-header.component";
+import { InfoBoxComponent } from '../../info-box/info-box.component';
+import { CompleteCharsComponent } from '../../complete-chars/complete-chars.component';
 
 @Component({
     selector: 'app-fruit-quiz',
     standalone: true,
     templateUrl: './fruit-quiz.component.html',
     styleUrl: './fruit-quiz.component.css',
-    imports: [GameHeaderComponent]
+    imports: [GameHeaderComponent, InfoBoxComponent, CompleteCharsComponent]
 })
 export class FruitQuizComponent {
+  tip1Count = 5;
+  tip2Count = 7;
+  tip3Count = 13;
+
+  headerFruit = 'QUE PERSONAGEM COMEU ESTE AKUMA NO MI ?';
+
 
 }
--- src/app/menu/wanted-quiz/wanted-quiz.component.ts
+++ src/app/menu/wanted-quiz/wanted-quiz.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { GameHeaderComponent } from "../../header/game-header/game-header.component";
+import { InfoBoxComponent } from '../../info-box/info-box.component';
+import { CompleteCharsComponent } from '../../complete-chars/complete-chars.component';
 
 @Component({
     selector: 'app-wanted-quiz',
     standalone: true,
     templateUrl: './wanted-quiz.component.html',
     styleUrl: './wanted-quiz.component.css',
-    imports: [GameHeaderComponent]
+    imports: [GameHeaderComponent, InfoBoxComponent, CompleteCharsComponent]
 })
 export class WantedQuizComponent {
+
+  headerWanted = 'QUE PERSONAGEM APARECE NESTE CARTAZ DE "WANTED"?'
 
 }
