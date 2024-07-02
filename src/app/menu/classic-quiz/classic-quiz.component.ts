fda0c821fc90c554aa4a75c0c87e7109d9b39270
Estêvão Roldão <estevaor_roldao@hotmail.com>
2024-07-02 15:54:12 -0300

    acho que fiz caca

--- src/app/menu/classic-quiz/classic-quiz.component.ts
+++ src/app/menu/classic-quiz/classic-quiz.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GameHeaderComponent } from '../../header/game-header/game-header.component';
 import { CompleteCharsComponent } from '../../complete-chars/complete-chars.component';
 import { InfoBoxComponent } from '../../info-box/info-box.component';
+import { personagens } from '../../personagens';
 
 @Component({
   selector: 'app-classic-quiz',
@@ -15,6 +16,8 @@ export class ClassicQuizComponent {
   headerSection = 'Adivinha qual é a personagem de One Piece de hoje!'
   gameStart = false;
   atemptsCount = 8;
+
+  choosenCharacter = personagens[4];
 
   constructor(){
     if(this.yesterDay === null){
--- src/app/personagens.ts
+++ src/app/personagens.ts
@@ -1,156 +1,612 @@
 export const personagens = [
   {
     name: 'Arlong',
-    path: 'Arlong.png'
-  },{
+    path: 'Arlong.png',
+    genero: 'homem',
+    filiacao: 'Piratas Arlong',
+    akumaNoMi: '',
+    haki: [''],
+    ultimaRecompensa: '20 mi',
+    altura:'2m63',
+    origem: 'Grand Line',
+    primeiroArco: 'East Blue'
+  },
+  {
     name: 'Bartholomew Kuma',
-    path: 'BartholomewKuma.png'
-  },{
+    path: 'BartholomewKuma.png',
+    genero: 'homem',
+    filiacao: 'Shichibukai',
+    akumaNoMi: 'Nikyu Nikyu no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '296 mi',
+    altura: '6m89',
+    origem: 'South Blue',
+    primeiroArco: 'Thriller Bark'
+  },
+  {
     name: 'Bellemere',
-    path: 'Bellemere.png'
-  },{
+    path: 'Bellemere.png',
+    genero: 'mulher',
+    filiacao: 'Marinha (ex)',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '1m86',
+    origem: 'East Blue',
+    primeiroArco: 'Arlong Park'
+  },
+  {
     name: 'Bentham',
-    path: 'Bentham.png'
-  },{
+    path: 'Bentham.png',
+    genero: 'homem',
+    filiacao: 'Baroque Works',
+    akumaNoMi: 'Mane Mane no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '32 mi',
+    altura: '2m38',
+    origem: 'South Blue',
+    primeiroArco: 'Little Garden'
+  },
+  {
     name: 'Boa Hancock',
-    path: 'BoaHancock.png'
-  },{
+    path: 'BoaHancock.png',
+    genero: 'mulher',
+    filiacao: 'Kuja, Shichibukai',
+    akumaNoMi: 'Mero Mero no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '80 mi',
+    altura: '1m91',
+    origem: 'Amazon Lily',
+    primeiroArco: 'Amazon Lily'
+  },
+  {
     name: 'Borsalino',
-    path: 'Borsalino.png'
-  },{
+    path: 'Borsalino.png',
+    genero: 'homem',
+    filiacao: 'Marinha',
+    akumaNoMi: 'Pika Pika no Mi (Logia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '3m02',
+    origem: 'North Blue',
+    primeiroArco: 'Sabaody Archipelago'
+  },
+  {
     name: 'Brogy',
-    path: 'Brogy.png'
-  },{
+    path: 'Brogy.png',
+    genero: 'homem',
+    filiacao: 'Gigantes Guerreiros',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '22m00',
+    origem: 'Elbaf',
+    primeiroArco: 'Little Garden'
+  },
+  {
     name: 'Brook',
-    path: 'Brook.png'
-  },{
+    path: 'Brook.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: 'Yomi Yomi no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '83 mi',
+    altura: '2m66',
+    origem: 'West Blue',
+    primeiroArco: 'Thriller Bark'
+  },
+  {
     name: 'Buggy',
-    path: 'Buggy.png'
-  },{
+    path: 'Buggy.png',
+    genero: 'homem',
+    filiacao: "Buggy's Delivery, Shichibukai",
+    akumaNoMi: 'Bara Bara no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '15 mi',
+    altura: '1m92',
+    origem: 'East Blue',
+    primeiroArco: 'Orange Town'
+  },
+  {
     name: 'Carrot',
-    path: 'Carrot.png'
-  },{
+    path: 'Carrot.png',
+    genero: 'mulher',
+    filiacao: 'Minks',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '1m61',
+    origem: 'Zou',
+    primeiroArco: 'Zou'
+  },
+  {
     name: 'Charlotte Katakuri',
-    path: 'CharlotteKatakuri.png'
-  },{
+    path: 'CharlotteKatakuri.png',
+    genero: 'homem',
+    filiacao: 'Piratas da Big Mom',
+    akumaNoMi: 'Mochi Mochi no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '1,057 bi',
+    altura: '5m09',
+    origem: 'Totto Land',
+    primeiroArco: 'Whole Cake Island'
+  },
+  {
     name: 'Charlotte Linlin',
-    path: 'CharlotteLinlin.png'
-  },{
+    path: 'CharlotteLinlin.png',
+    genero: 'mulher',
+    filiacao: 'Piratas da Big Mom',
+    akumaNoMi: 'Soru Soru no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '4,388 bi',
+    altura: '8m80',
+    origem: 'Elbaf',
+    primeiroArco: 'Whole Cake Island'
+  },
+  {
     name: 'Charlotte Pudding',
-    path: 'CharlottePudding.png'
-  },{
+    path: 'CharlottePudding.png',
+    genero: 'mulher',
+    filiacao: 'Piratas da Big Mom',
+    akumaNoMi: 'Memo Memo no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '1m66',
+    origem: 'Totto Land',
+    primeiroArco: 'Whole Cake Island'
+  },
+  {
     name: 'Crocodile',
-    path: 'Crocodile.png'
-  },{
-    name: 'DonKrieg',
-    path: 'DonKrieg.png'
-  },{
+    path: 'Crocodile.png',
+    genero: 'homem',
+    filiacao: 'Baroque Works',
+    akumaNoMi: 'Suna Suna no Mi (Logia)',
+    haki: [],
+    ultimaRecompensa: '81 mi',
+    altura: '2m53',
+    origem: 'Grand Line',
+    primeiroArco: 'Alabasta'
+  },
+  {
+    name: 'Don Krieg',
+    path: 'DonKrieg.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Krieg',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '17 mi',
+    altura: '2m63',
+    origem: 'East Blue',
+    primeiroArco: 'Baratie'
+  },
+  {
     name: 'Donquixote Doflamingo',
-    path: 'DonquixoteDoflamingo.png'
-  },{
+    path: 'DonquixoteDoflamingo.png',
+    genero: 'homem',
+    filiacao: 'Donquixote Pirates, Shichibukai',
+    akumaNoMi: 'Ito Ito no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '340 mi',
+    altura: '3m05',
+    origem: 'North Blue',
+    primeiroArco: 'Dressrosa'
+  },
+  {
     name: 'Donquixote Rosinante',
-    path: 'DonquixoteRosinante.png'
-  },{
+    path: 'DonquixoteRosinante.png',
+    genero: 'homem',
+    filiacao: 'Marinha',
+    akumaNoMi: 'Nagi Nagi no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '2m98',
+    origem: 'North Blue',
+    primeiroArco: 'Dressrosa'
+  },
+  {
     name: 'Dorry',
-    path: 'Dorry.png'
-  },{
+    path: 'Dorry.png',
+    genero: 'homem',
+    filiacao: 'Gigantes Guerreiros',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '21m00',
+    origem: 'Elbaf',
+    primeiroArco: 'Little Garden'
+  },
+  {
     name: 'Dracule Mihawk',
-    path: 'DraculeMihawk.png'
-  },{
+    path: 'DraculeMihawk.png',
+    genero: 'homem',
+    filiacao: 'Shichibukai',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '1m98',
+    origem: 'Grand Line',
+    primeiroArco: 'Baratie'
+  },
+  {
     name: 'Edward Newgate',
-    path: 'EdwardNewgate.png'
-  },{
+    path: 'EdwardNewgate.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Barba Branca',
+    akumaNoMi: 'Gura Gura no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '5,046 bi',
+    altura: '6m66',
+    origem: 'Grand Line',
+    primeiroArco: 'Marineford'
+  },
+  {
     name: 'Enel',
-    path: 'Enel.png'
-  },{
+    path: 'Enel.png',
+    genero: 'homem',
+    filiacao: 'Deus de Skypiea',
+    akumaNoMi: 'Goro Goro no Mi (Logia)',
+    haki: ['observação'],
+    ultimaRecompensa: '',
+    altura: '2m66',
+    origem: 'Birka',
+    primeiroArco: 'Skypiea'
+  },
+  {
     name: 'Eustass Kid',
-    path: 'EustassKid.png'
-  },{
+    path: 'EustassKid.png',
+    genero: 'homem',
+    filiacao: 'Kid Pirates',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '470 mi',
+    altura: '2m05',
+    origem: 'South Blue',
+    primeiroArco: 'Sabaody Archipelago'
+  },
+  {
     name: 'Franky',
-    path: 'Franky.png'
-  },{
+    path: 'Franky.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '94 mi',
+    altura: '2m40',
+    origem: 'South Blue',
+    primeiroArco: 'Water 7'
+  },
+  {
     name: 'Gin',
-    path: 'Gin.png'
-  },{
-    name: 'Gol D Roger',
-    path: 'GolDRoger.png'
-  },{
+    path: 'Gin.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Krieg',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    primeiroArco: 'Baratie'
+  },
+  {
+    name: 'Gol D. Roger',
+    path: 'GolDRoger.png',
+    genero: 'homem',
+    filiacao: 'Roger Pirates',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '5,564 bi',
+    altura: '2m74',
+    origem: 'East Blue',
+    primeiroArco: 'Loguetown'
+  },
+  {
     name: 'Jewelry Bonney',
-    path: 'JewelryBonney.png'
-  },{
-    name: 'Jinbe',
-    path: 'Jinbe.png'
-  },{
+    path: 'JewelryBonney.png',
+    genero: 'mulher',
+    filiacao: 'Bonney Pirates',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '140 mi',
+    altura: '1m74',
+    origem: 'South Blue',
+    primeiroArco: 'Sabaody Archipelago'
+  },
+  {
+  name: 'Jinbe',
+    path: 'Jinbe.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '438 mi',
+    altura: '3m01',
+    origem: 'Fishman Island',
+    primeiroArco: 'Arlong Park'
+  },
+  {
     name: 'Kaido',
-    path: 'Kaido.png'
-  },{
+    path: 'Kaido.png',
+    genero: 'homem',
+    filiacao: 'Beasts Pirates',
+    akumaNoMi: 'Uo Uo no Mi, Model: Seiryu (Zoan)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '4,611 bi',
+    altura: '7m10',
+    origem: 'Grand Line',
+    primeiroArco: 'Wano'
+  },
+  {
     name: 'Kikunojo',
-    path: 'Kikunojo.png'
-  },{
+    path: 'Kikunojo.png',
+    genero: 'mulher',
+    filiacao: 'Nove Bainhas Vermelhas',
+    akumaNoMi: '',
+    haki: ['armamento'],
+    ultimaRecompensa: '',
+    altura: '2m87',
+    origem: 'Wano',
+    primeiroArco: 'Wano'
+  },
+  {
     name: 'Kinemon',
-    path: 'Kinemon.png'
-  },{
+    path: 'Kinemon.png',
+    genero: 'homem',
+    filiacao: 'Nove Bainhas Vermelhas',
+    akumaNoMi: 'Fuku Fuku no Mi (Paramecia)',
+    haki: ['armamento'],
+    ultimaRecompensa: '',
+    altura: '2m77',
+    origem: 'Wano',
+    primeiroArco: 'Punk Hazard'
+  },
+  {
     name: 'Kureha',
-    path: 'Kureha.png'
-  },{
+    path: 'Kureha.png',
+    genero: 'mulher',
+    filiacao: 'Ilha Drum',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '1m70',
+    origem: 'Grand Line',
+    primeiroArco: 'Drum Island'
+  },
+  {
     name: 'Kuzan',
-    path: 'Kuzan.png'
-  },{
-    name: 'Marshall D Teach',
-    path: 'MarshallDTeach.png'
-  },{
-    name: 'Monkey D Dragon',
-    path: 'MonkeyDDragon.png'
-  },{
-    name: 'Monkey D Garp',
-    path: 'MonkeyDGarp.png'
-  },{
-    name: 'Monkey D Luffy',
-    path: 'MonkeyDLuffy.png'
-  },{
+    path: 'Kuzan.png',
+    genero: 'homem',
+    filiacao: 'Marinha (ex)',
+    akumaNoMi: 'Hie Hie no Mi (Logia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '2m98',
+    origem: 'South Blue',
+    primeiroArco: 'Long Ring Long Land'
+  },
+  {
+    name: 'Marshall D. Teach',
+    path: 'MarshallDTeach.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Barba Negra',
+    akumaNoMi: 'Yami Yami no Mi (Logia), Gura Gura no Mi (Paramecia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '2,247 bi',
+    altura: '3m44',
+    origem: 'South Blue',
+    primeiroArco: 'Jaya'
+  },
+  {
+    name: 'Monkey D. Dragon',
+    path: 'MonkeyDDragon.png',
+    genero: 'homem',
+    filiacao: 'Revolutionary Army',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '',
+    altura: '2m66',
+    origem: 'East Blue',
+    primeiroArco: 'Loguetown'
+  },
+  {
+    name: 'Monkey D. Garp',
+    path: 'MonkeyDGarp.png',
+    genero: 'homem',
+    filiacao: 'Marinha',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '2m87',
+    origem: 'East Blue',
+    primeiroArco: 'Water 7'
+  },
+  {
+    name: 'Monkey D. Luffy',
+    path: 'MonkeyDLuffy.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: 'Gomu Gomu no Mi (Paramecia)',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '1,500 bi',
+    altura: '1m74',
+    origem: 'East Blue',
+    primeiroArco: 'Romance Dawn'
+  },
+  {
     name: 'Nami',
-    path: 'Nami.png'
-  },{
+    path: 'Nami.png',
+    genero: 'mulher',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '66 mi',
+    altura: '1m70',
+    origem: 'East Blue',
+    primeiroArco: 'Orange Town'
+  },
+  {
     name: 'Nico Robin',
-    path: 'NicoRobin.png'
-  },{
-    name: 'Portgas D Ace',
-    path: 'PortgasDAce.png'
-  },{
+    path: 'NicoRobin.png',
+    genero: 'mulher',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: 'Hana Hana no Mi (Paramecia)',
+    haki: [],
+    ultimaRecompensa: '130 mi',
+    altura: '1m88',
+    origem: 'West Blue',
+    primeiroArco: 'Whiskey Peak'
+  },
+  {
+    name: 'Portgas D. Ace',
+    path: 'PortgasDAce.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Barba Branca',
+    akumaNoMi: 'Mera Mera no Mi (Logia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '550 mi',
+    altura: '1m85',
+    origem: 'East Blue',
+    primeiroArco: 'Drum Island'
+  },
+  {
     name: 'Rob Lucci',
-    path: 'RobLucci.png'
-  },{
+    path: 'RobLucci.png',
+    genero: 'homem',
+    filiacao: 'CP9',
+    akumaNoMi: 'Neko Neko no Mi, Model: Leopard (Zoan)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '2m12',
+    origem: 'North Blue',
+    primeiroArco: 'Water 7'
+  },
+  {
     name: 'Roronoa Zoro',
-    path: 'RoronoaZoro.png'
-  },{
+    path: 'RoronoaZoro.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '320 mi',
+    altura: '1m81',
+    origem: 'East Blue',
+    primeiroArco: 'Romance Dawn'
+  },
+  {
     name: 'Sabo',
-    path: 'Sabo.png'
-  },{
+    path: 'Sabo.png',
+    genero: 'homem',
+    filiacao: 'Revolutionary Army',
+    akumaNoMi: 'Mera Mera no Mi (Logia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '602 mi',
+    altura: '1m87',
+    origem: 'East Blue',
+    primeiroArco: 'Post-Enies Lobby'
+  },
+  {
     name: 'Sakazuki',
-    path: 'Sakazuki.png'
-  },{
+    path: 'Sakazuki.png',
+    genero: 'homem',
+    filiacao: 'Marinha',
+    akumaNoMi: 'Magu Magu no Mi (Logia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '3m06',
+    origem: 'North Blue',
+    primeiroArco: 'Post-Enies Lobby'
+  },
+  {
     name: 'Sengoku',
-    path: 'Sengoku.png'
-  },{
+    path: 'Sengoku.png',
+    genero: 'homem',
+    filiacao: 'Marinha',
+    akumaNoMi: 'Hito Hito no Mi, Modelo: Daibutsu (Zoan)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '',
+    altura: '2m78',
+    origem: 'North Blue',
+    primeiroArco: 'Marineford'
+  },
+{
     name: 'Shimotsuki Ryuma',
-    path: 'ShimotsukiRyuma.png'
-  },{
+    path: 'ShimotsukiRyuma.png',
+    genero: 'homem',
+    filiacao: 'Samurai',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '1m79',
+    origem: 'Wano',
+    primeiroArco: 'Thriller Bark'
+  },
+  {
     name: 'Silvers Rayleigh',
-    path: 'SilversRayleigh.png'
-  },{
+    path: 'SilversRayleigh.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Roger',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação', 'rei'],
+    ultimaRecompensa: '',
+    altura: '1m88',
+    origem: 'Grand Line',
+    primeiroArco: 'Sabaody Archipelago'
+  },
+  {
     name: 'Tony Tony Chopper',
-    path: 'TonyTonyChopper.png'
-  },{
-    name: 'Trafalgar D Water Law',
-    path: 'TrafalgarDWaterLaw.png'
-  },{
+    path: 'TonyTonyChopper.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: 'Hito Hito no Mi (Zoan)',
+    haki: [],
+    ultimaRecompensa: '100',
+    altura: '0m90',
+    origem: 'Grand Line',
+    primeiroArco: 'Drum Island'
+  },
+  {
+    name: 'Trafalgar D. Water Law',
+    path: 'TrafalgarDWaterLaw.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Coração',
+    akumaNoMi: 'Ope Ope no Mi (Paramecia)',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '500 mi',
+    altura: '1m91',
+    origem: 'North Blue',
+    primeiroArco: 'Sabaody Archipelago'
+  },
+  {
     name: 'Usopp',
-    path: 'Usopp.png'
-  },{
+    path: 'Usopp.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: ['observação'],
+    ultimaRecompensa: '200 mi',
+    altura: '1m76',
+    origem: 'East Blue',
+    primeiroArco: 'Syrup Village'
+  },
+  {
     name: 'Vinsmoke Sanji',
-    path: 'VinsmokeSanji.png'
-  },{
+    path: 'VinsmokeSanji.png',
+    genero: 'homem',
+    filiacao: 'Piratas do Chapéu de Palha',
+    akumaNoMi: '',
+    haki: ['armamento', 'observação'],
+    ultimaRecompensa: '330 mi',
+    altura: '1m80',
+    origem: 'North Blue',
+    primeiroArco: 'Baratie'
+  },
+  {
     name: 'Zeff',
-    path: 'Zeff.png'
+    path: 'Zeff.png',
+    genero: 'homem',
+    filiacao: 'Baratie',
+    akumaNoMi: '',
+    haki: [],
+    ultimaRecompensa: '',
+    altura: '2m00',
+    origem: 'East Blue',
+    primeiroArco: 'Baratie'
   }
-]
+];
