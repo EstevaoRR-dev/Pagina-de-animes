388e9b019d5d0ac3ed0d68c71f692e323068d24d
Estêvão Roldão <estevaor_roldao@hotmail.com>
2024-07-04 14:52:08 -0300

    Adicionei o conteudo do quizz da risada, juntamente com a logica. Ainda falta alguns refinamentos.

--- src/app/menu/laugh-quiz/laugh-quiz.component.ts
+++ src/app/menu/laugh-quiz/laugh-quiz.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, viewChild } from '@angular/core';
 import { GameHeaderComponent } from "../../header/game-header/game-header.component";
+import { CompleteCharsComponent } from '../../complete-chars/complete-chars.component';
+import { InfoBoxComponent } from '../../info-box/info-box.component';
+import { gifs } from '../../scenesGifs';
+import { Personagem } from '../../personagensBck';
 
 @Component({
     selector: 'app-laugh-quiz',
     standalone: true,
     templateUrl: './laugh-quiz.component.html',
     styleUrl: './laugh-quiz.component.css',
-    imports: [GameHeaderComponent]
+    imports: [GameHeaderComponent, CompleteCharsComponent, InfoBoxComponent]
 })
 export class LaughQuizComponent {
+  blurNumber = 8;
+  imgBlur = 'blur(' + this.blurNumber + 'px) grayscale(1)';
+  gifsScenes = gifs;
+  todayGif = this.gifsScenes[Math.floor(Math.random() * 8)];
+
+  charSelected(char:Personagem){
+    if(!(char.nome.toLowerCase() === this.todayGif.nome.toLowerCase())){
+      console.log(char.nome.toLowerCase() + this.todayGif.nome.toLowerCase());
+      this.blurNumber-= 1;
+      this.imgBlur = 'blur(' + this.blurNumber + 'px) grayscale(1)';
+    }
+    else{
+      alert('Você acertou!!!');
+      this.blurNumber = 1;
+      this.imgBlur = 'none';
+    }
+  }
 
 }
---
+++ src/app/scenesGifs.ts
@@ -0,0 +1,26 @@
+export const gifs = [
+  {
+    path: '../assets/images/zoro01.webp',
+    nome: 'Roronoa Zoro'
+  },
+  {
+    path: '../assets/images/desconhecido.webp',
+    nome: 'Jewelry Bonney'
+  },
+  {
+    path: '../assets/images/law.webp',
+    nome: 'Trafalgar D. Water Law'
+  },
+  {
+    path: '../assets/images/law2.webp',
+    nome: 'Trafalgar D. Water Law',
+  },
+  {
+    path: '../assets/images/sanji.webp',
+    nome: 'Vinsmoke Sanji'
+  },
+  {
+    path: '../assets/images/sanji2.webp',
+    nome: 'Vinsmoke Sanji'
+  }
+]
